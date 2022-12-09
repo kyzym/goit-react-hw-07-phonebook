1bb23fe799acda6edb36354ea8b69928fcbefc05
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-12-09 16:29:19 +0100

    add visebleContacts selector

--- src/components/ContactList/ContactsList.jsx
+++ src/components/ContactList/ContactsList.jsx
@@ -1,18 +1,10 @@
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 import { Box } from '../utils/Box.styled';
 import { ContactsListItem } from './ContactListItem/ContactsListItem';
 
 export const ContactsList = () => {
-  const contacts = useSelector(selectContacts);
-
-  const filter = useSelector(selectFilter);
-
-  const normalizedFilter = filter.toLowerCase().trim();
-
-  const filteredContacts = contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
+  const filteredContacts = useSelector(selectVisibleContacts);
 
   return (
     <Box as="ul" width="430px">
